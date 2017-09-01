3f920ea9886eb93c55e0eefdf2e6fb3d3a404c94
SeedBoot <oliboots@gmail.com>
2017-09-01 10:34:49 +0100

    created program to make folders and folders in folders

--- ex3.py
+++ ex3.py
@@ -1,3 +1,31 @@
+"""Generates directories for screenshots"""
 import os
 
-os.mkdir("path")
+############################
+# variables: DO NOT DELETE #
+############################
+EN_FOLDER = "EN"
+FR_FOLDER = "FR"
+#Rename these if needed#
+CATEGORIES = [
+    "_Homescreens", "1 Getting Started", "2 Device Software",
+    "3 Email, Contacts,,,", "4 Sync", "5 Usage",
+    "6 Trouble", "7 Wifi", "8 Travelling"
+]
+
+##############################################
+# creates initial directories: DO NOT DELETE #
+##############################################
+os.mkdir(EN_FOLDER)
+os.mkdir(FR_FOLDER)
+os.mkdir("Statusbar")
+
+###############################################
+# Loops that make directories from CATEGORIES #
+#  inside EN and FR directories respectively  #
+###############################################
+for folder in CATEGORIES:
+    os.mkdir(os.path.join(EN_FOLDER, folder))
+
+for folder in CATEGORIES:
+    os.mkdir(os.path.join(FR_FOLDER, folder))
